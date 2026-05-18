ddbbca9729daffddffc28e3d820e56a3e7904039
sruthi939 <sathiyasuru@gmail.com>
2026-05-18 17:37:29 +0530

    Creating the components

---
+++ frontend/src/components/TrailerSection.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import ReactPlayer from 'react-player'
+import BlurCircle from '../components/BlurCircle'
+
+const TrailerSection = () => {
+
+    const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0])
+
+    return (
+        <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
+            <p className='text-gray-300 font-medium text-lg max-w-240 mx-auto'>Trailers</p>
+            <div className='relative mt-6'>
+                <BlurCircle top='-100px' right='-100px' />
+                <ReactPlayer url={currentTrailer.videoUrl} control={false}
+                    className='mx-auto max-w-full'
+                    width='960px'
+                    height='540px'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default TrailerSection
