71db32ed4f749aed24ac51ae737336718e189144
sruthi939 <sathiyasuru@gmail.com>
2026-05-18 14:37:11 +0530

    Complete Navbar Section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { assets } from '../assets/assets'
 import { MenuIcon, Search, TicketPlus, X } from 'lucide-react'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
@@ -15,7 +14,7 @@ const Navbar = () => {
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
       <Link to='/' className='max-md: flex-1'>
-        <img src={assets.logo} alt='logo' className='w-36 h-auto' />
+        <img src="" alt='logo' className='w-36 h-auto' />
       </Link>
       <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 md:px-8 py-3 max-md:h-screen md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transistion-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
         <X className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
