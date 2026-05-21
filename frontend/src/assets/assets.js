0df130602db01b763cd45d599f63fe3a09d51e30
sruthi939 <sathiyasuru@gmail.com>
2026-05-21 19:36:54 +0530

    Adding assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,19 @@
+import logo from 'logo.png';
+import movie1 from 'movie1.jpeg';
+import movie2 from 'movie2.jpeg';
+import movie3 from 'movie3.jpeg';
+import movie4 from 'movie4.jpeg';
+import movie5 from 'movie5.jpeg';
+import movie6 from 'movie6.jpeg';
+import HeroSection from 'HeroSection.png';
+
+export const assets = {
+    logo,
+    HeroSection,
+    movie1,
+    movie2,
+    movie3,
+    movie4,
+    movie5,
+    movie6,
+}
